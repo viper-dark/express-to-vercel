4c75db37178e8571640e8adfe869e01c53dfbe21
Elmehdi Akiri <74506295+viper-dark@users.noreply.github.com>
2023-07-30 21:50:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,52 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-   const puppeteer = require("puppeteer-core");
-const chromium = require("@sparticuz/chromium-min");
+// file.mjs (ES module)
+import express from 'express';
+import path from 'path';
 
-app.use(express.static('public'))
+const app = express();
+app.use(express.static('public'));
 
 app.get('/', async (req, res) => {
- 
+  const handler = async () => {
+    try {
+      const puppeteer = (await import('puppeteer-core')).default;
+      const chromium = (await import('@sparticuz/chromium-min')).default;
 
-const handler = async () => {
-  try {
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(
-        "https://github.com/Sparticuz/chromium/releases/download/v110.0.1/chromium-v110.0.1-pack.tar"
-      ),
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
+      const browser = await puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath(
+          "https://github.com/Sparticuz/chromium/releases/download/v110.0.1/chromium-v110.0.1-pack.tar"
+        ),
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+      });
+
+      const page = await browser.newPage();
+
+      await page.goto("https://www.google.com/", { waitUntil: "networkidle0" });
+
+      console.log("Chromium:", await browser.version());
+      console.log("Page Title:", await page.title());
+
+      await page.close();
+
+      await browser.close();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  handler()
+    .then(() => {
+      console.log("Done");
+      res.status(200).send("ERROR no link or data found for match ");
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
     });
-
-    const page = await browser.newPage();
-
-    await page.goto("https://www.google.com/", { waitUntil: "networkidle0" });
-
-    console.log("Chromium:", await browser.version());
-    console.log("Page Title:", await page.title());
-
-    await page.close();
-
-    await browser.close();
-  } catch (error) {
-    throw new Error(error.message);
-  }
-};
-
-handler().then(() => {console.log("Done")
-                       res.status(200).send(
-        "ERROR no link or data found for match "
-      );
-                     });
-   
-})
+});
 
 app.listen(process.env.PORT || 3000);
 
-module.exports = app;
+export default app;
